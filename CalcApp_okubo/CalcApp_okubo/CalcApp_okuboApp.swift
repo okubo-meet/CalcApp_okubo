8e856e54961b7d43867fe403ee9db2d80229857c
大久保徹郎 <okubo@office-meet.com>
2021-09-29 15:15:55 +0900

    プロジェクト立ち上げ

---
+++ CalcApp_okubo/CalcApp_okubo/CalcApp_okuboApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp_okuboApp.swift
+//  CalcApp_okubo
+//
+//  Created by 大久保徹郎 on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp_okuboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
